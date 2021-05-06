b4c7562f506a1730767584e362946770d3cd5e05
Ankur Sharma <sharma18ankur@gmail.com>
2021-05-06 19:02:26 +0530

    final commit for all funtionalities

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
-  return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
-  )
-}
+   const { cocktails, loading } = useGlobalContext();
+   if (loading) {
+      return <Loading />;
+   }
+   if (cocktails.length < 1) {
+      return (
+         <h2 className="section-title">
+            No result found for your "KEYWORDS"!!
+         </h2>
+      );
+   }
+   return (
+      <section className="section">
+         <h2 className="sectio-title">cocktails</h2>
+         <div className="cocktails-center">
+            {cocktails.map((item) => {
+               return <Cocktail key={item.id} {...item} />;
+            })}
+         </div>
+      </section>
+   );
+};
 
-export default CocktailList
+export default CocktailList;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React, { useEffect, useRef } from "react";
+import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
-}
+   const { setSearchTerm } = useGlobalContext();
+   const refSearchValue = useRef("");
 
-export default SearchForm
+   useEffect(() => {
+    refSearchValue.current.focus()
+   }, [])
+
+   const searchCocktail = () => {
+      setSearchTerm(refSearchValue.current.value);
+   };
+    const handleSubmit = (e) => {
+      e.preventDefault()
+    }
+   return (
+      <section className="section search">
+         <form className="search-form" onSubmit={handleSubmit}>
+            <div className="form-control">
+               <label htmlFor="name">Search your cocktail by NAME</label>
+               <input
+                  type="text"
+                  id="name"
+                  ref={refSearchValue}
+                  onChange={searchCocktail}
+               />
+            </div>
+         </form>
+      </section>
+   );
+};
+
+export default SearchForm;
