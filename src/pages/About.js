ecf8353e0e3a1f7e1a3d36f0efc1f36549970835
Ankur Sharma <sharma18ankur@gmail.com>
2021-05-06 10:17:17 +0530

    Added Navbar with About & Error page with routing

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+     <section className="section about-section">
+        <h1 className="section-title">About us</h1>
+        <p>
+           Components are the heart of React's powerful, declarative programming model.
+           React Router is a collection of navigational components that compose
+           declaratively with your application. Whether you want to have bookmarkable URLs
+           for your web app or a composable way to navigate in React Native, React Router
+           works wherever React is rendering--so take your pick!
+        </p>
+     </section>
+  );
+}
+
+export default About
